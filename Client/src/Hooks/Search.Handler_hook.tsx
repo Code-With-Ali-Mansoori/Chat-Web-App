c14652390b1837891f4601897bd573cb54ade807
Ali Mansoori <aliahmedmansoori090@gmail.com>
2026-04-14 13:29:10 +0530

    Searching feature in ChatUI is Done

--- Client/src/Hooks/Search.Handler_hook.tsx
+++ Client/src/Hooks/Search.Handler_hook.tsx
@@ -1,15 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import Search_user_Function from '../helper/Search.user.ts'
-import useDebounced_search from "./Debounse.search.tsx";
 
 const useUser_Search = (search : string) => {
     
-    const Debounced_searchValue = useDebounced_search(search); //Debounce Search
-
     return useQuery({
             queryKey : ['searched_user', search],
-            queryFn  : () => Search_user_Function(Debounced_searchValue),
-            enabled  : Debounced_searchValue?.trim().length > 0 // only runs when search exists
+            queryFn  : () => Search_user_Function(search),
+            enabled  : search?.trim().length > 0 // only runs when search exists
     });
 };
 
--- Client/src/components/Left_chatUi.tsx
+++ Client/src/components/Left_chatUi.tsx
@@ -2,11 +2,21 @@ import { Search } from "lucide-react"
 import Mychats from "./mychats"
 import { useNavigate } from "react-router-dom";
 import useProfile_Hooks from "../Hooks/Profile.Hook";
+import useSearch from "../Hooks/SearchContext.hook";
+import useUser_Search from "../Hooks/Search.Handler_hook";
+import MySearchResults from "./Search_res_help";
+import type { Key } from "react";
+import type { UserSearchResult } from "../Context/context";
+import { useState } from "react";
 
 const Left_chatUi = () => {
 
   const navigator =  useNavigate();
   const { data } = useProfile_Hooks();
+
+  const { setUserSearched, userSearched } = useSearch();
+  const [searchTrigger, setSearchTrigger] = useState<string>('');
+  const {data : searchedRes} = useUser_Search(searchTrigger);  
 
   return (
     <div className=" h-full w-full">
@@ -20,15 +30,38 @@ const Left_chatUi = () => {
             </div>
             
             <div className='flex bg-blue-00  pr-3 justify-start items-center w-4/4 h-full'>
-                <input className='border-2 border-gray-300 w-2/3 p-2 pl-3 rounded-l-2xl focus:outline-none' type="text" placeholder='Search here..'/>
-                <button className='border-2 flex justify-center items-center  rounded-r-2xl w-2/8  hover:cursor-pointer border-gray-600 md:px-4 px-2 p-3 bg-gray-600 hover:bg-gray-500 hover:border-gray-500 text-white font-mono'> 
+                <input value={userSearched} onChange={(e) => { setUserSearched(e.target.value); setSearchTrigger(''); }} className='border-2 border-gray-300 w-2/3 p-2 pl-3 rounded-l-2xl focus:outline-none' type="text" placeholder='Search here..'/>
+
+                <button onClick={() => setSearchTrigger(userSearched)} className='border-2 flex justify-center items-center  rounded-r-2xl w-2/8  hover:cursor-pointer border-gray-600 md:px-4 px-2 p-3 bg-gray-600 hover:bg-gray-500 hover:border-gray-500 text-white font-mono'> 
                     <Search className="text-white font-medium" size={16} strokeWidth={2.50} />
                 </button>
             </div>
         </div>
 
         <div className="border p-3 overflow-y-scroll overflow-x-hidden chat-scroll  border-gray-500 h-6/7  mt-4 m-4 rounded-2xl">
-            <Mychats/>
+            {searchTrigger.trim() && searchedRes ? (
+                Array.isArray(searchedRes.message) && searchedRes.message.length > 0 ? (
+                    searchedRes.message.map((user : UserSearchResult , index : Key) => (
+                        <MySearchResults
+                            key={index}
+                            username={user.username}
+                            Active_Status={user.Active_Status}
+                            Last_active={user.Last_active}
+                            user_avatar={user.user_avatar}
+                            _id={user._id}
+                            Public_user_id={user.Public_user_id}
+                            user_Bio={user.user_Bio}
+                            user_gender={user.user_gender}
+                        />
+                    ))
+                ) : searchedRes.message === "User not Found" ? (
+                    <div className="text-center text-gray-500 mt-4">No users found</div>
+                ) : (
+                    <Mychats/>
+                )
+            ) : (
+                <Mychats/>
+            )}
         </div>
     </div>
   )
